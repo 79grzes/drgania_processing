284325145f29d283c4f5dbbc98b31db309e2f6a4
79grzes <g.koziel@gmail.com>
2025-01-03 10:45:20 +0100

    First commit (test one)

---
+++ tdms_read_test.py
@@ -0,0 +1,14 @@
+from nptdms import TdmsFile
+
+tdms_file = TdmsFile.read("14.03.2015.tdms")
+for group in tdms_file.groups():
+    group_name = group.name
+    for channel in group.channels():
+        channel_name = channel.name
+        # Access dictionary of properties:
+        properties = channel.properties
+        # Access numpy array of data for channel:
+        data = channel[:]
+        # Access a subset of data
+        data_subset = channel[10:20]
+        print(data_subset)
